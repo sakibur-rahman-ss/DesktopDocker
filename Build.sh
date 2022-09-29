02405aaba7eb0436a3c4b006fdb72acd052d58a0
Sakibur Rahman <94929392+sakibur-rahman-ss@users.noreply.github.com>
2022-09-29 11:35:36 -0700

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -2,4 +2,7 @@
 set -x
 git clone git@github.com:setschedule/set-desktop.git
 docker build -t stgdesktop .
+docker ps --filter "status=exited" | cut -d' ' -f1 | grep -v CONTAINER | xargs -n1 docker rm
+DesktopContainer=$(docker ps --all --filter "name=stgdesktop1" | wc -l)
+echo $DesktopContainer
 docker run -d --name stgdesktop1 -p 3000:3000 stgdesktop
